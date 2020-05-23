087d43c89d65ac83d63630648171a3fd9bd9ed5f
鸡毛 <shihao.gu@perfma.com>
2020-05-23 09:58:51 +0800

    更新注释

--- src/hash/index.ts
+++ src/hash/index.ts
@@ -1,5 +1,9 @@
 import Hasha from 'hasha';
 
+/**
+ * 提供对于文本进行hash计算的方法
+ * @param text 文本
+ */
 export function Hash(text: string): string {
   return Hasha(text, {
     algorithm: 'sha1',
--- src/valueType/index.ts
+++ src/valueType/index.ts
@@ -1,9 +1,16 @@
+/**
+ * 此枚举描述了一个用于过渡的中间类型系统，和JavaScript数据一对一映射
+ */
 export enum ValueType {
+  // 一下五个类型为基础类型，其值可以直接用作TypeScript类型名称
+  // Unknow代表了undefined，null以及其他所有不能识别出类型的数据的类型
   Unknow = 'any',
   Boolean = 'boolean',
   Number = 'number',
   String = 'string',
   Date = 'Date',
-  Object = '_object',
-  Array = '_array',
+  // Record代表一个键值对映射（对象）
+  Record = '_record',
+  // List后期会演化成为数组或元组
+  List = '_list',
 }
