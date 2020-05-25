210944137c829f99466a88ed7bbf5e98e709f9f2
鸡毛 <shihao.gu@perfma.com>
2020-05-25 18:20:08 +0800

    提交

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,35 @@
 import rspObj from '../test/2.json';
-import { Value } from './value';
 import { Field } from './field';
-import Lodash from 'lodash';
 import { Type } from './type';
 import fs from 'fs';
 import path from 'path';
 import { IntfDef } from './intfDef';
 
-
-function writeIntfToFile(
-  intf: IntfDef,
-  output: string,
-): void {
-  intf.DepSubIntfDefs.forEach((intf) => {
-    writeIntfToFile(intf, path.resolve(output, intf.DirName));
-  });
-  if (output) {
-    if (!fs.existsSync(output)) {
-      fs.mkdirSync(output, { recursive: true });
-    }
-    const codeFilePath = path.resolve(output, 'index.ts');
-    fs.writeFileSync(codeFilePath, intf.IntfCode.Code, 'utf8');
-  }
-}
-
-function Do(
-  value: any,
-  name: string = '',
-  outpath?: string,
-): Type {
-  const field = new Field(value, name);
-  if (outpath) {
-    field.Type.IntfDefs.forEach((intf) => {
-      writeIntfToFile(intf, outpath);
+export class Prober {
+  private writeIntfToFile(
+    intf: IntfDef,
+    output: string,
+  ): void {
+    intf.DepSubIntfDefs.forEach((intf) => {
+      this.writeIntfToFile(intf, path.resolve(output, intf.DirName));
     });
+    if (output) {
+      if (!fs.existsSync(output)) {
+        fs.mkdirSync(output, { recursive: true });
+      }
+      const codeFilePath = path.resolve(output, 'index.ts');
+      fs.writeFileSync(codeFilePath, intf.IntfCode.Code, 'utf8');
+    }
   }
-  return field.Type;
-}
 
-Do(rspObj.object, 'rsp', path.resolve(__dirname, 'output'));
+  public Do(
+    value: any,
+    desc: string = '',
+    outpath: string = '',
+  ): Type {
+    const field = new Field(value, desc);
+    return field.Type;
+  }
+
+  public constructor() {}
+}
