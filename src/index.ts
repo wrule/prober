f6f7a9e402d313be944d61d5146592f17af4008c
鸡毛 <shihao.gu@perfma.com>
2020-05-25 21:25:18 +0800

    writeIntfDefToFile方法review

--- src/index.ts
+++ src/index.ts
@@ -5,20 +5,30 @@ import fs from 'fs';
 import path from 'path';
 import { IntfDef } from './intfDef';
 
+/**
+ * 探测器类
+ */
 export class Prober {
-  private writeIntfToFile(
-    intf: IntfDef,
-    output: string,
+  /**
+   * 写入某一接口定义的代码到指定路径的文件
+   * @param intfDef 接口定义
+   * @param outPath 文件写入路径
+   */
+  private writeIntfDefToFile(
+    intfDef: IntfDef,
+    outPath: string,
   ): void {
-    intf.DepSubIntfDefs.forEach((intf) => {
-      this.writeIntfToFile(intf, path.resolve(output, intf.DirName));
+    // 深度优先，生成依赖代码
+    intfDef.DepSubIntfDefs.forEach((depIntfDef) => {
+      const depDirPath = path.join(outPath, depIntfDef.DirName);
+      this.writeIntfDefToFile(depIntfDef, depDirPath);
     });
-    if (output) {
-      if (!fs.existsSync(output)) {
-        fs.mkdirSync(output, { recursive: true });
+    if (outPath) {
+      if (!fs.existsSync(outPath)) {
+        fs.mkdirSync(outPath, { recursive: true });
       }
-      const codeFilePath = path.resolve(output, 'index.ts');
-      fs.writeFileSync(codeFilePath, intf.IntfCode.Code, 'utf8');
+      const codeFilePath = path.join(outPath, 'index.ts');
+      fs.writeFileSync(codeFilePath, intfDef.IntfCode.Code);
     }
   }
 
