6aa555d9c08b2d136abac33618ce2ff5e708c744
鸡毛 <shihao.gu@perfma.com>
2020-05-26 17:34:07 +0800

    review intfCode

--- src/intfCode/index.ts
+++ src/intfCode/index.ts
@@ -1,6 +1,12 @@
 import { IntfDef } from "../intfDef";
 
+/**
+ * 用于生成接口代码的类
+ */
 export class IntfCode {
+  /**
+   * 根据内置模板获取接口TypeScript代码
+   */
   public get Code(): string {
     return `
 ${this.intfDef.DepSubIntfDefs.map((intf) => `import { ${intf.Name} } from './${intf.DirName}';`).join('\r\n')}
@@ -8,9 +14,13 @@ ${this.intfDef.DepSubIntfDefs.map((intf) => `import { ${intf.Name} } from './${i
 export interface ${this.intfDef.Name} {
 ${this.intfDef.Fields.map((field) => `  '${field.SrcName}': ${field.Type.TypeDesc};`).join('\r\n')}  
 }
-`;
+`.trim() + '\r\n';
   }
 
+  /**
+   * 构造函数
+   * @param intfDef 接口定义
+   */
   public constructor(
     private intfDef: IntfDef,
   ) {}
