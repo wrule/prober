29c4db7c4e3493460f5f891a587d5662d3d87903
鸡毛 <shihao.gu@perfma.com>
2020-05-27 19:03:35 +0800

    是否需要参数数组化?

--- src/type/index.ts
+++ src/type/index.ts
@@ -2,46 +2,27 @@ import Lodash from 'lodash';
 import { Value } from '../value';
 import { ValueType } from '../valueType';
 import { TypeKind } from '../typeKind';
+import { Hash } from '../hash';
 
 export class Type {
-  // 数据是只有一种类型的列表
-  // 元组是有多种类型的列表
-
-  // 类型可以是单个基本类型
-  // 类型可以是接口类型
-  // 类型可以是多个类型并列
-  // 类型可以是某一个类型构成的数组类型
-  // 类型可以是多个类型构成的元组类型
-
-
   private kind: TypeKind;
   /**
-   * 类型种类
+   * 类型的种类
    */
   public get Kind(): TypeKind {
     return this.kind;
   }
 
-  private types: Type[] = [];
   /**
-   * 类型依赖的类型
+   * 此类型是否为基础类型
    */
-  public get Types(): Type[] {
-    return this.types;
-  }
-
-  private typeExps: string = '';
-  /**
-   * 类型表达式（可直接用于TypeScript代码）
-   */
-  public get TypeExps(): string {
-    return `(${this.typeExps})`;
-  }
-
   public get IsBase(): boolean {
-    return this.kind !== TypeKind.Interface &&
+    return (
+      this.kind !== TypeKind.Interface &&
+      this.kind !== TypeKind.Union &&
       this.kind !== TypeKind.Array &&
-      this.kind !== TypeKind.Tuple;
+      this.kind !== TypeKind.Tuple
+    );
   }
 
   private getInterfaceName(
@@ -52,6 +33,107 @@ export class Type {
     suffixsText = suffixsText ? `_${suffixsText}` : '';
     const name = Lodash.upperFirst(Lodash.camelCase(desc));
     return `I${name}${suffixsText}`;
+  }
+
+  private getUnionName(types: Type[]): string {
+    return types.map((type) => type.TypeExps).join(' | ');
+  }
+
+  private getArrayName(types: Type[]): string {
+    return `${this.types[0].TypeExps}[]`;
+  }
+
+  private getTupleName(types: Type[]): string {
+    return `[${types.map((type) => type.TypeExps).join(', ')}]`;
+  }
+
+  /**
+   * 获取数组的类型
+   * @param values 值列表
+   * @param desc 描述
+   * @param suffixs 后缀描述
+   */
+  private getArrayTypes(
+    values: Value[],
+    desc: string = '',
+    suffixs: string[] = [],
+  ): Type[] {
+    return [new Type(values[0], desc, suffixs.concat(['ArrayItem']))];
+  }
+
+  /**
+   * 获取元组的所有类型
+   * @param values 值列表
+   * @param desc 描述
+   * @param suffixs 后缀描述
+   */
+  private getTupleTypes(
+    values: Value[],
+    desc: string = '',
+    suffixs: string[] = [],
+  ): Type[] {
+    return values.map((value, index) => 
+      new Type(value, desc, suffixs
+        .concat([`TupleItem${index + 1}x${value.StructHash.slice(0, 4).toUpperCase()}`]))
+    );
+  }
+
+  /**
+   * 判断多个值的结构hash是否一致
+   * @param values 值列表
+   */
+  private hashIsConsistent(values: Value[]): boolean {
+    return Array.from(new Set(values.map((value) => value.StructHash))).length < 2;
+  }
+
+  private types: Type[] = [];
+
+  public get TypeExps(): string {
+    let result = '';
+    if (this.IsBase) {
+      result = this.kind;
+    } else {
+      switch (this.kind) {
+        case TypeKind.Interface: {
+          result = this.getInterfaceName(this.desc, this.suffixs);
+        } break;
+        case TypeKind.Union: {
+          result = this.getUnionName(this.types);
+        } break;
+        case TypeKind.Array: {
+          result = this.getArrayName(this.types);
+        } break;
+        case TypeKind.Tuple: {
+          result = this.getTupleName(this.types);
+        } break;
+        default:;
+      }
+    }
+    return `(${result})`;
+  }
+
+
+  public get TypeHash(): string {
+    if (this.kind === TypeKind.Union) {
+      return Hash(this.types.map((type) => type.TypeHash).join('|'));
+    } else {
+      return this.value.StructHash;
+    }
+  }
+
+
+  /**
+   * 为此类型联合一个新的类型
+   * @param newType 
+   */
+  public UniteType(newType: Type): void {
+    if (this.kind === TypeKind.Union) {
+
+    } else {
+      const bkType = {...this};
+      this.types = [bkType, newType];
+    }
+    this.kind = TypeKind.Union;
   }
 
   /**
@@ -68,29 +150,31 @@ export class Type {
     switch (this.value.Type) {
       case ValueType.Boolean: {
         this.kind = TypeKind.Boolean;
-        this.typeExps = TypeKind.Boolean;
       } break;
       case ValueType.Number: {
         this.kind = TypeKind.Number;
-        this.typeExps = TypeKind.Number;
       } break;
       case ValueType.String: {
         this.kind = TypeKind.String;
-        this.typeExps = TypeKind.String;
       } break;
       case ValueType.Date: {
         this.kind = TypeKind.Date;
-        this.typeExps = TypeKind.Date;
       } break;
       case ValueType.Record: {
         this.kind = TypeKind.Interface;
-        this.typeExps = this.getInterfaceName(this.desc, this.suffixs);
       } break;
       case ValueType.List: {
-        this.kind = TypeKind.Array;
-        if (this.value.List.length > 0) {
-
+        const list = this.value.List;
+        if (list.length > 0) {
+          if (this.hashIsConsistent(this.value.List)) {
+            this.kind = TypeKind.Array;
+            this.types = this.getArrayTypes(list, this.desc, this.suffixs);
+          } else {
+            this.kind = TypeKind.Tuple;
+            this.types = this.getTupleTypes(list, this.desc, this.suffixs);
+          }
         } else {
+          this.kind = TypeKind.Array;
           this.types = [new Type(new Value(null))];
         }
       } break;
--- src/typeKind/index.ts
+++ src/typeKind/index.ts
@@ -8,8 +8,13 @@ export enum TypeKind {
     Number = 'number',
     String = 'string',
     Date = 'Date',
+    // 接口类型
     Interface = '_interface',
+    // 联合类型
+    Union = '_union',
+    // 数组类型
     Array = '_array',
+    // 元组类型
     Tuple = '_tuple',
   }
   
