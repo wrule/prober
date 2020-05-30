4612ab8b9aaed79e28d1a19dc82312241d3450bf
鸡毛 <shihao.gu@perfma.com>
2020-05-30 16:50:43 +0800

    思考合并的各种策略

--- src/type/array/index.ts
+++ src/type/array/index.ts
@@ -1,6 +1,7 @@
 import { Type } from '../index';
 import { TypeKind } from '../../typeKind';
 import { Hash } from '../../hash';
+import { TypeUnion } from '../union';
 
 export class TypeArray extends Type {
   public get IsBase(): boolean {
@@ -16,7 +17,30 @@ export class TypeArray extends Type {
     return this.hash;
   }
 
+  public get ArrayItemType(): Type {
+    return this.types[0];
+  }
+
   public DiffMerge(type: Type): Type {
+    if (type.IsBase) {
+      return new TypeUnion([this, type]);
+    } else {
+      switch (type.Kind) {
+        case TypeKind.Interface: {
+          return new TypeUnion([this, type]);
+        } break;
+        case TypeKind.Union: {
+          return new TypeUnion([this, type]);
+        } break;
+        case TypeKind.Array: {
+          const dstType = type as TypeArray;
+          return new TypeArray([this.ArrayItemType.Merge(dstType.ArrayItemType)]);
+        } break;
+        case TypeKind.Tuple: {
+
+        } break;
+      }
+    }
     return this;
   }
 
