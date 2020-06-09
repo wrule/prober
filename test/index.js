50a20b4b67eb7e5579224f0da08228797f968e13
gu <1982775886@qq.com>
2020-06-09 23:35:04 +0800

    初步测试功能达到预期

--- test/index.js
+++ test/index.js
@@ -1,9 +1,9 @@
 const path = require('path');
-const obj = require('./a.json');
+const obj = require('./1.json');
 const { Prober } = require('../dist');
 
 const dstPath = path.join(__dirname, '..', 'src', 'output');
 const prober = new Prober();
-const newType = prober.Update(obj, 'rsp', dstPath);
+const newType = prober.Update(obj.object, 'rsp', dstPath);
 console.log(newType.TypeDesc);
 
