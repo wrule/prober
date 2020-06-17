e110d3eec2b1b564f5a8027644acbe8291e1e786
gu <1982775886@qq.com>
2020-06-17 21:50:54 +0800

    两个枚举的注释

--- src/typeKind/index.ts
+++ src/typeKind/index.ts
@@ -3,20 +3,49 @@
  */
 export enum TypeKind {
     // 以下七个类型为基础类型，其值可以直接用作TypeScript类型名称
+    /**
+     * 未知的类型
+     */
     Any = 'any',
+    /**
+     * null类型
+     */
     Null = 'null',
+    /**
+     * undefined类型
+     */
     Undefined = 'undefined',
+    /**
+     * boolean类型
+     */
     Boolean = 'boolean',
+    /**
+     * number类型
+     */
     Number = 'number',
+    /**
+     * string类型
+     */
     String = 'string',
+    /**
+     * Date类型
+     */
     Date = 'Date',
-    // 接口类型
+    /**
+     * 接口类型
+     */
     Interface = '_interface',
-    // 联合类型
+    /**
+     * 联合类型
+     */
     Union = '_union',
-    // 数组类型
+    /**
+     * 数组类型
+     */
     Array = '_array',
-    // 元组类型
+    /**
+     * 元组类型
+     */
     Tuple = '_tuple',
   }
   
--- src/valueType/index.ts
+++ src/valueType/index.ts
@@ -4,15 +4,40 @@
 export enum ValueType {
   // 以下七个类型为基础类型，其值可以直接用作TypeScript类型描述
   // Unknow代表了不能识别出类型的数据的类型
+  /**
+   * 未知类型，any
+   */
   Unknow = 'any',
+  /**
+   * null类型
+   */
   Null = 'null',
+  /**
+   * undefined类型
+   */
   Undefined = 'undefined',
+  /**
+   * boolean类型
+   */
   Boolean = 'boolean',
+  /**
+   * number类型
+   */
   Number = 'number',
+  /**
+   * string类型
+   */
   String = 'string',
+  /**
+   * date类型
+   */
   Date = 'Date',
-  // Record代表一个键值对映射（对象）
+  /**
+   * Record代表一个键值对映射（对象）
+   */
   Record = '_record',
-  // List为数据列表，后期会演化成为数组或元组
+  /**
+   * List为数据列表，后期会演化成为数组或元组
+   */
   List = '_list',
 }
