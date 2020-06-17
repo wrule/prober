9dfd041dc1cfa28a5e3c0ba8a521229c128b2afe
鸡毛 <shihao.gu@perfma.com>
2020-06-17 10:49:08 +0800

    加入options

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,14 @@ import { TypeJSON } from './typeJson';
 import { Type } from './type';
 import { TypeInterface } from './type/interface';
 import { TypeKind } from './typeKind';
+import { ICodeOptions } from './codeOptions';
 
 /**
  * 探测器类
  */
 export class Prober {
+  private readonly TypeFileName: string = 'type.json';
+
   /**
    * 向指定目录写入文本文件
    * @param outPath 目录
@@ -33,15 +36,17 @@ export class Prober {
    * 向指定目录写入接口类型定义代码
    * @param outPath 指定目录
    * @param intfType 接口类型
+   * @param options 代码生成选项
    */
   private writeIntf(
     outPath: string,
     intfType: TypeInterface,
+    options: ICodeOptions,
   ): void {
     intfType.DepIntfTypes.forEach((type) => {
-      this.writeIntf(path.join(outPath, type.IntfFullName), type);
+      this.writeIntf(path.join(outPath, type.IntfFullName), type, options);
     });
-    const intfCode = new IntfCode(intfType);
+    const intfCode = new IntfCode(intfType, options);
     this.writeFile(outPath, 'index.ts', intfCode.Code);
   }
 
@@ -49,33 +54,25 @@ export class Prober {
    * 探测JavaScript的值的类型
    * @param value JavaScript值
    * @param desc 类型描述（一般为字段名）
-   * @param outPath 代码输出目录的路径
    */
   public Do(
     value: any,
     desc: string = '',
-    outPath: string = '',
   ): Type {
     const field = new Field(value, desc);
     const deducer = new TypeDeducer();
-    const type = deducer.Deduce(field.Value, field.SrcName);
-    if (outPath) {
-      this.Dump(type, outPath);
-    }
-    return type;
+    return deducer.Deduce(field.Value, field.SrcName);
   }
 
   /**
    * 从本地加载类型
    * @param inPath 输入路径
-   * @param fileName 目标文件名
    * @returns 加载得到的类型
    */
   public Load(
     inPath: string,
-    fileName: string = 'type.json',
   ): Type {
-    const filePath = path.join(inPath, fileName);
+    const filePath = path.join(inPath, this.TypeFileName);
     const jsonText = fs.readFileSync(filePath, 'utf8');
     return TypeJSON.Parse(jsonText);
   }
@@ -88,15 +85,15 @@ export class Prober {
   public Dump(
     type: Type,
     outPath: string,
-    fileName: string = 'type.json',
+    options: ICodeOptions,
   ): void {
     const jsonStr = TypeJSON.Stringify(type);
-    this.writeFile(outPath, fileName, jsonStr);
+    this.writeFile(outPath, this.TypeFileName, jsonStr);
     if (type.Kind === TypeKind.Interface) {
-      this.writeIntf(outPath, type as TypeInterface);
+      this.writeIntf(outPath, type as TypeInterface, options);
     } else {
       type.DepIntfTypes.forEach((dtype) => {
-        this.writeIntf(path.join(outPath, dtype.IntfFullName), dtype);
+        this.writeIntf(path.join(outPath, dtype.IntfFullName), dtype, options);
       });
     }
   }
@@ -109,9 +106,8 @@ export class Prober {
    */
   public Exists(
     outPath: string,
-    fileName: string = 'type.json',    
   ): boolean {
-    const filePath = path.join(outPath, fileName);
+    const filePath = path.join(outPath, this.TypeFileName);
     return fs.existsSync(filePath);
   }
 
@@ -119,19 +115,21 @@ export class Prober {
    * 更新类型
    * @param value JavaScript值
    * @param desc 类型描述（一般为字段名）
-   * @param outPath 代码目标路径
+   * @param dstPath 代码目标路径
+   * @param options 代码生成选项
    */
   public Update(
     value: any,
     desc: string = '',
     dstPath: string = '',
+    options: ICodeOptions,
   ): Type {
     let newType = this.Do(value, desc);
     if (this.Exists(dstPath)) {
       const oldType = this.Load(dstPath);
       newType = oldType.Merge(newType);
     }
-    this.Dump(newType, dstPath);
+    this.Dump(newType, dstPath, options);
     return newType;
   }
 
