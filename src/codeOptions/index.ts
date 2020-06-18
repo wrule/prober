cbbac117fe4107e86d2d7a04ab34a50bb3466b9f
鸡毛 <shihao.gu@perfma.com>
2020-06-18 09:46:09 +0800

    配置项新增,但是还没写具体的业务逻辑

--- src/codeOptions/index.ts
+++ src/codeOptions/index.ts
@@ -5,9 +5,17 @@ export interface ICodeOptions {
   /**
    * 接口成员可省略
    */
-  mbrOmit: boolean;
+  mbrOmit?: boolean;
   /**
    * 可添加任意接口成员
    */
-  anyMbr: boolean;
+  anyMbr?: boolean;
+  /**
+   * 数组元素采样率，(0, 1]，默认为1，即百分百采样
+   */
+  arraySamplingRate?: number;
+  /**
+   * 数组元素采样个数，与采样率冲突，优先级高于采样率
+   */
+  arraySamplingCount?: number;
 }
